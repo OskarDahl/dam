778bb25df8fb99fb90b8e09dff6233575c3afe8f
Aron Sai <aron.sai@gmail.com>
2014-03-30 13:37:08 +0200

    pre-alpha 4
    420

---
+++ Assets/Scripts/selectPiece.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class selectPiece : MonoBehaviour {
+
+	bool green = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetButton("Fire1"))
+        {
+			if (green == false)
+			{
+				show();
+				green = true;
+			}
+			else
+			{
+				hide();
+			}
+        }
+	}
+}
---
+++ Assets/Scripts/showGreen.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class showGreen : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void show() {
+		gameObject.SetActive (true);
+	}
+
+	public void hide() {
+		gameObject.SetActive (false);
+	}
+}
