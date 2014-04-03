c3d4b6cccdbf595e00fad505f76e05749a6eeee0
OskarDahl <gulgroda@gmail.com>
2014-04-03 17:28:20 +0200

    pre-alpha 5
    Fixade show/hide för move när man klickar

--- Assets/Scripts/showGreen.cs
+++ Assets/Scripts/showGreen.cs
@@ -1,23 +1,39 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class showGreen : MonoBehaviour {
+public class showGreen : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    bool sh = false;
+    
 
-	}
+    // Use this for initialization
+    void Start()
+    {
 
-	public void show() {
-		gameObject.SetActive (true);
-	}
+    }
 
-	public void hide() {
-		gameObject.SetActive (false);
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            if (sh == true)
+            {
+                renderer.enabled = false;
+                //gameObject.SetActive(false);
+                sh = false;
+                System.Threading.Thread.Sleep(200);
+            }
+            else if (sh == false)
+            {
+                renderer.enabled = true;
+                //gameObject.SetActive(true);
+                sh = true;
+                System.Threading.Thread.Sleep(200);
+            }
+        }
+
+    }
+
 }
